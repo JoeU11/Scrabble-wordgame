34684769d98def9755e41afb50b961d05872f51f
joe <joe.ulica@gmail.com>
2023-07-13 08:56:32 -0400

    finish and test calculateHandlen

--- ps4a.py
+++ ps4a.py
@@ -196,7 +196,10 @@ def calculateHandlen(hand):
     hand: dictionary (string-> int)
     returns: integer
     """
-    # TO DO... <-- Remove this comment when you code this function
+    count = 0
+    for letter in hand:
+        count += hand[letter]
+    return count
 
 
 def playHand(hand, wordList, n):
