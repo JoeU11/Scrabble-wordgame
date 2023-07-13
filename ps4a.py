dce47bb2fbbf198ba6bbcdcb1c9dbdba73a888df
joe <joe.ulica@gmail.com>
2023-07-13 09:29:08 -0400

    finish playHand

--- ps4a.py
+++ ps4a.py
@@ -143,7 +143,7 @@ def updateHand(hand, word):
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
+    many of that letter in it.
 
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
@@ -151,7 +151,7 @@ def updateHand(hand, word):
     Has no side effects: does not modify hand.
 
     word: string
-    hand: dictionary (string -> int)    
+    hand: dictionary (string -> int)
     returns: dictionary (string -> int)
     """
     handCopy = hand.copy()
@@ -190,7 +190,7 @@ def isValidWord(word, hand, wordList):
 #
 
 def calculateHandlen(hand):
-    """ 
+    """
     Returns the length (number of letters) in the current hand.
 
     hand: dictionary (string-> int)
@@ -207,7 +207,7 @@ def playHand(hand, wordList, n):
     Allows the user to play the given hand, as follows:
 
     * The hand is displayed.
-    * The user may input a word or a single period (the string ".") 
+    * The user may input a word or a single period (the string ".")
       to indicate they're done playing
     * Invalid words are rejected, and a message is displayed asking
       the user to choose another word until they enter a valid word or "."
@@ -224,32 +224,39 @@ def playHand(hand, wordList, n):
       n: integer (HAND_SIZE; i.e., hand size required for additional points)
 
     """
-    # BEGIN PSEUDOCODE <-- Remove this comment when you code this function; do your coding within the pseudocode (leaving those comments in-place!)
     # Keep track of the total score
-
+    score = 0
     # As long as there are still letters left in the hand:
+    while calculateHandlen(hand) > 0:
 
-    # Display the hand
-
-    # Ask user for input
-
-    # If the input is a single period:
-
-    # End the game (break out of the loop)
-
-    # Otherwise (the input is not a single period):
-
-    # If the word is not valid:
-
-    # Reject invalid word (print a message followed by a blank line)
-
-    # Otherwise (the word is valid):
-
-    # Tell the user how many points the word earned, and the updated total score, in one line followed by a blank line
-
-    # Update the hand
-
+        # Display the hand
+        print("Current Hand:", end=" ")
+        displayHand(hand)
+        # Ask user for input
+        word = input("Enter word, or a '.' to indicate that you are finished:")
+        # If the input is a single period:
+        if word == ".":
+            # End the game (break out of the loop)
+            break
+        # Otherwise (the input is not a single period):
+        else:
+            # If the word is not valid:
+            if not isValidWord(word, hand, wordList):
+                # Reject invalid word (print a message followed by a blank line)
+                print("Invalid word, please try again.")
+                print("")
+            # Otherwise (the word is valid):
+            else:
+                # Tell the user how many points the word earned, and the updated total score, in one line followed by a blank line
+                wordScore = getWordScore(word, n)
+                score += wordScore
+                print("'" + word + "' earned " + str(wordScore) +
+                      " points. Total: " + str(score) + " points")
+                # Update the hand
+                print("")
+                hand = updateHand(hand, word)
     # Game is over (user entered a '.' or ran out of letters), so tell user the total score
+    print("Goodbye! Total score: " + str(score) + " points.")
 
 
 #
--- test_ps4a.py
+++ test_ps4a.py
@@ -236,3 +236,22 @@ print("Testing calculateHandlen...")
 test_calculateHandlen()
 print("----------------------------------------------------------------------")
 print("All done!")
+
+
+# Testing playHand
+
+# Test 1
+#wordList = loadWords()
+#playHand({'h': 1, 'i': 1, 'c': 1, 'z': 1, 'm': 2, 'a': 1}, wordList, 7)
+
+# Test 2
+# wordList = loadWords()
+# playHand({'w': 1, 's': 1, 't': 2, 'a': 1, 'o': 1, 'f': 1}, wordList, 7)
+
+# test 3
+# wordList = loadWords()
+# playHand({'n': 1, 'e': 1, 't': 1, 'a': 1, 'r': 1, 'i': 2}, wordList, 7)
+
+# test 4
+wordList = loadWords()
+playHand({'n': 1, 'e': 1, 't': 1, 'a': 1, 'r': 1}, wordList, 5)
